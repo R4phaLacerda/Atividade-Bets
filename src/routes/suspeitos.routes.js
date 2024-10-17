b907ac56ae2bb454da311f207387d165e3adbf74
raphaelle lacerda <raphaellelacerda.tds2.senai@gmail.com>
2024-10-17 08:19:52 -0300

    feat: suspeitos.routes.js all methods

---
+++ src/routes/suspeitos.routes.js
@@ -0,0 +1,129 @@
+import { Router } from "express";
+
+const suspeitosRoutes = Router();
+
+// Array com suspeitos pré-cadastrados
+let suspeitos = [
+  {
+    id: Math.floor(Math.random() * 1000000),
+  } 
+  
+]
+
+// Rota para listar todos os suspeitos
+suspeitosRoutes.get("/", (req, res) => {
+  return res.status(200).json(suspeitos);
+});
+
+// Rota para cadastrar um novo suspeito
+suspeitosRoutes.post("/", (req, res) => {
+  const { nome, partido, idade, segundo, propostas } = req.body;
+
+  // Validação dos campos nome e partido
+  if (!nome || !partido) {
+    return res.status(400).send({
+      message: "O nome ou o partido não foi preenchido, criança aleatória!",
+    });
+  }
+
+  // Validação de idade
+  if (idade < 18) {
+    return res.status(400).send({
+      message:
+        "O suspeito não possui idade suficiente para participar deste debate!",
+    });
+  }
+
+  // Criação de um novo suspeito
+  const novosuspeito = {
+    id: Math.floor(Math.random() * 1000000),
+    nome,
+    partido,
+    idade,
+    segundo,
+    propostas,
+  };
+
+  // Adiciona o novo suspeito ao array de suspeitos
+  suspeitos.push(novosuspeito);
+
+  return res.status(201).json({
+    message: "suspeito cadastrado com sucesso!",
+    novosuspeito,
+  });
+});
+
+// Rota para buscar um suspeito pelo id
+suspeitosRoutes.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  // Busca um suspeito pelo id no array de suspeitos
+  const suspeito = suspeitos.find((acusado) => acusado.id == id);
+
+  // Verifica se o suspeito foi encontrado
+  if (!suspeito) {
+    return res
+      .status(404)
+      .json({ message: `suspeito com id ${id} não encontrado!` });
+  }
+
+  return res.status(200).json(suspeito);
+});
+
+// Rota para atualizar um suspeito pelo id
+suspeitosRoutes.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { nome, partido, idade, segundo, propostas } = req.body;
+
+  // Busca um suspeito pelo id no array de suspeitos
+  const suspeito = suspeitos.find((acusado) => acusado.id == id);
+
+  // Verifica se o suspeito foi encontrado
+  if (!suspeito) {
+    return res
+      .status(404)
+      .json({ message: `suspeito com id ${id} não encontrado!` });
+  }
+
+  // Validação dos campos nome e partido
+  if (!nome || !partido) {
+    return res.status(400).send({
+      message: "O nome ou o partido não foi preenchido, criança aleatória!",
+    });
+  }
+
+  suspeito.nome = nome;
+  suspeito.partido = partido;
+  suspeito.idade = idade;
+  suspeito.segundo = segundo;
+  suspeito.propostas = propostas;
+
+  return res.status(200).json({
+    message: "suspeito atualizado com sucesso!",
+    suspeito,
+  });
+});
+
+suspeitosRoutes.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  // Busca um suspeito pelo id no array de suspeitos
+  const suspeito = suspeitos.find((acusado) => acusado.id == id);
+
+  // Verifica se o suspeito foi encontrado
+  if (!suspeito) {
+    return res
+      .status(404)
+      .json({ message: `suspeito com id ${id} não encontrado!` });
+  }
+
+  // Remove o suspeito do array de suspeitos
+  suspeitos = suspeitos.filter((suspeito) => suspeito.id != id);
+
+  return res.status(200).json({
+    message: "suspeito removido com sucesso!",
+    suspeito,
+  });
+});
+
+export default suspeitosRoutes;
